3fbd20ce3cb869a5f55699ebd888ef51fd60b6d2
Pratiksha K Jadhav <72095693+pratikshakj@users.noreply.github.com>
2022-07-09 00:22:14 +0530

    Add files via upload

---
+++ career/views.py
@@ -0,0 +1,135 @@
+from django.shortcuts import render
+
+# Create your views here.
+def career10(request):
+    return render(request,'career/10th.html')
+
+def index(request):
+    return render(request,'career/index.html')
+
+def index1(request):
+    return render(request,'career/index1.html')
+
+def index2(request):
+    return render(request,'career/index2.html')
+
+def tree(request):
+    return render(request,'class12/tree.html')
+
+def cse(request):
+    return render(request,'class12/cse.html')
+
+def mech(request):
+    return render(request,'class12/mech.html')
+
+def eee(request):
+    return render(request,'class12/eee.html')
+
+def civil(request):
+    return render(request,'class12/civil.html')
+
+def chem(request):
+    return render(request,'class12/chem.html')
+
+def ise(request):
+    return render(request,'class12/ise.html')
+
+def aero(request):
+    return render(request,'class12/aero.html')
+
+def biotech(request):
+    return render(request,'class12/biotech.html')
+
+def enc(request):
+    return render(request,'class12/enc.html')
+
+def autorobot(request):
+    return render(request,'class12/autorobot.html')
+
+def mbbs(request):
+    return render(request,'class12/mbbs.html')
+
+def bds(request):
+    return render(request,'class12/bds.html')
+
+def bams(request):
+    return render(request,'class12/bams.html')
+
+def bums(request):
+    return render(request,'class12/bums.html')
+
+def bhms(request):
+    return render(request,'class12/bhms.html')
+
+def bnys(request):
+    return render(request,'class12/bnys.html')
+
+def bvsc(request):
+    return render(request,'class12/bvsc.html')
+
+
+def bscIT(request):
+    return render(request,'class12/bscIT.html')
+
+def bscCS(request):
+    return render(request,'class12/bscCS.html')
+
+def bscHospital(request):
+    return render(request,'class12/bscHospital.html')
+
+def bscBiotech(request):
+    return render(request,'class12/bscBiotech.html')
+
+def bscAnime(request):
+    return render(request,'class12/bscAnime.html')
+
+def bscFashion(request):
+    return render(request,'class12/bscFashion.html')
+
+def bscAgri(request):
+    return render(request,'class12/bscAgri.html')
+
+def bca(request):
+    return render(request,'class12/bca.html')
+
+def bscNurse(request):
+    return render(request,'class12/bscNurse.html')
+
+def bcom(request):
+    return render(request,'class12/bcom.html')
+
+def bba(request):
+    return render(request,'class12/bba.html')
+
+def ca(request):
+    return render(request,'class12/ca.html')
+
+def bms(request):
+    return render(request,'class12/bms.html')
+
+def bbs(request):
+    return render(request,'class12/bbs.html')
+
+def bhm(request):
+    return render(request,'class12/bhm.html')
+
+def ba(request):
+    return render(request,'class12/ba.html')
+
+def bfa(request):
+    return render(request,'class12/bfa.html')
+
+def bjmc(request):
+    return render(request,'class12/bjmc.html')
+
+def baLLB(request):
+    return render(request,'class12/baLLB.html')
+
+def bfd(request):
+    return render(request,'class12/bfd.html')
+
+def pg(request):
+    return render(request,'career/pg.html')
+
+def careerHome(request):
+    return render(request,'career/cghome.html')
